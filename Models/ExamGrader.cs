921aebe27e61b97e6b418d9b341c723acb204a0b
Akash santhosh <akash777kk@gmail.com>
2023-08-11 16:53:22 +0530

    Magic number

---
+++ Models/ExamGrader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvoidMagicNumber.Models
+{
+    internal class ExamGrader
+    {
+        private const int ExcellentScore = 90;
+        private const int GoodScore = 75;
+        private const int AverageScore = 50;
+
+        public  string GradeExam(int score)
+        {
+            if (score >= ExcellentScore)
+            {
+                return "Excellent";
+            }
+            else if (score >= GoodScore)
+            {
+                return "Good";
+            }
+            else if (score >= AverageScore)
+            {
+                return "Average";
+            }
+            else
+            {
+                return "Below Average";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using AvoidMagicNumber.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvoidMagicNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Magic numbers are hard-coded numeric values used directly in code without explanation.
+
+            int studentScore = 60;
+            ExamGrader exam=new ExamGrader();
+            //use named constants to replace magic numbers.
+            string grade =exam.GradeExam(studentScore);
+            // string grade =exam.GradeExam(90); example for magic number
+            Console.WriteLine($"Student's score: {studentScore}");
+            Console.WriteLine($"Grade: {grade}");
+
+
+            DayOfWeek currentDay = DayOfWeek.Sunday;
+
+            if (currentDay == DayOfWeek.Sunday || currentDay == DayOfWeek.Saturday)
+            {
+                Console.WriteLine("It's the weekend! Time to relax.");
+            }
+            else
+            {
+                Console.WriteLine("It's a weekday. Time to work.");
+            }
+            
+        }
+        //Avoid Hardcoded Thresholds:
+        //Using magic numbers may lead to typos or misunderstandings
+        
+
+
+    }
+    public class Working
+    {
+        public enum DayOfWeek { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday }
+
+        //Use Enums for Discrete Values:
+        // Enums represent a set of related named constants.
+
+    }
+}
+    
